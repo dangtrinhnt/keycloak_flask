55dd335d849ea47d41c0991265c7769077a5ab8c
travis-infosec <travis@sidepocket.com>
2021-12-21 21:21:34 -0600

    defaults

--- keycloak_flask/settings.py
+++ keycloak_flask/settings.py
@@ -1,12 +1,15 @@
 # for local
+# It's probably helpful for us to demonstrate what the URL should be, etc.
 
-SECRET_KEY = b''
+SECRET_KEY = b'keycloak'
 
-SERVER_URL = ""
-ADMIN_USERNAME = ""
-ADMIN_PASS = ""
-REALM_NAME = ""
-CLIENT_ID = ""
+# http, not https for some reason
+SERVER_URL = "http://keycloak-idp:8080/auth/"
+ADMIN_USERNAME = "admin"
+ADMIN_PASS = "admin"
+REALM_NAME = "master"
+# created in keycloak per https://github.com/keycloak/keycloak-documentation/blob/main/securing_apps/topics/client-registration/client-registration-cli.adoc
+CLIENT_ID = "keycloak-flask"
 CLIENT_SECRET = ""
 
-INGRESS_HOST = ""
+INGRESS_HOST = "https://www.google.com/"
