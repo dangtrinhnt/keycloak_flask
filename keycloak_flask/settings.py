f588070ca6d6a90317466f16f878a94bc84e6879
travis-infosec <travis@sidepocket.com>
2021-12-21 21:40:45 -0600

    exmaple settings

--- keycloak_flask/settings.py
+++ keycloak_flask/settings.py
@@ -10,6 +10,7 @@ ADMIN_PASS = "admin"
 REALM_NAME = "master"
 # created in keycloak per https://github.com/keycloak/keycloak-documentation/blob/main/securing_apps/topics/client-registration/client-registration-cli.adoc
 CLIENT_ID = "keycloak-flask"
-CLIENT_SECRET = ""
+# set access-type to confidential, save, reload, will see a credentials tab where you can set this
+CLIENT_SECRET = "2da4a9a4-f6f0-48d9-82f6-12012402f03a"
 
-INGRESS_HOST = "https://www.google.com/"
+INGRESS_HOST = "http://www.google.com/"
